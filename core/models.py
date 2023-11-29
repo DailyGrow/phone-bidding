a1f0dfdd77000a90e8cd0e9854b8a4df21cd90f1
ZeyuCui <64547984+ZeyuCui@users.noreply.github.com>
2023-11-28 21:37:23 -0800

    edited model

--- core/models.py
+++ core/models.py
@@ -133,7 +133,7 @@ class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=9, decimal_places=2, validators=[MinValueValidator(0.01)], default=100, null=True)
-
+    tracking_number = models.CharField(max_length=50, blank=True, null=True)
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
 
